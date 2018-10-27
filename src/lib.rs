5a69139ed97d765410a06279c1f528c7830f5774
Marko Mijalkovic <marko.mijalkovic97@gmail.com>
2018-10-27 16:37:44 -0400

    Add support for recursive locking

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-//! Type-level safe mutable global access.
+//! Type-level safe mutable global access, with support for recursive immutable
+//! locking.
 //!
 //! ```
 //! use global::Global;
@@ -11,7 +12,7 @@
 //!
 //! for _ in 0..100 {
 //!     threads.push(std::thread::spawn(|| {
-//!         *VALUE.lock() += 1;
+//!         *VALUE.lock_mut().unwrap() += 1;
 //!     }));
 //! }
 //!
@@ -20,31 +21,61 @@
 //! }
 //!
 //! // This value is guaranteed to be 100.
-//! assert_eq!(*VALUE.lock(), 100);
+//! assert_eq!(*VALUE.lock().unwrap(), 100);
 //! ```
 
-extern crate once_nonstatic;
+extern crate parking_lot;
 
 use std::{
-    sync::{Arc, Mutex, MutexGuard},
+    fmt,
+    error::Error,
+    sync::Arc,
     ops::{Deref, DerefMut},
-    cell::UnsafeCell,
+    cell::{self, UnsafeCell, RefCell},
     mem::ManuallyDrop,
 };
 
-use once_nonstatic::Once;
+use parking_lot::{Once, ONCE_INIT, ReentrantMutex, ReentrantMutexGuard};
+
+/// A failure occured while borrowing the global value.
+///
+/// This happens when the value is incorrectly borrowed twice within a single
+/// thread. While cross-thread locking will simply block until the value is
+/// available, within a single thread potential deadlocks are detected. An error
+/// can occur in one of three ways:
+///
+/// * Attempting to immutably borrow a value that is already mutably borrowed.
+/// * Attempting to mutably borrow a value that is already mutably borrowed.
+/// * Attempting to mutably borrow a value that is already immutably borrowed.
+///
+/// Note that this error will not be raised in the case of multiple immutable
+/// borrows.
+#[derive(Debug)]
+pub struct BorrowFail;
+
+impl fmt::Display for BorrowFail {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "failed to borrow global value twice in same thread")
+    }
+}
+
+impl Error for BorrowFail {}
+
+/// A value slot stored inside of `Global<T>`.
+type InnerPointer<T> = Option<Arc<ReentrantMutex<RefCell<T>>>>;
 
 /// A global value.
 ///
 /// All types wrapped in `Global` must implement [`Default`]. This gives the
 /// initial value of the global variable.
 ///
-/// Handles to this value can be obtained with the `Global::lock` method.
+/// Handles to this value can be obtained with the `Global::lock` or
+/// `Global::lock_mut` methods.
 ///
 /// [`Default`]: https://doc.rust-lang.org/std/default/trait.Default.html
 pub struct Global<T> {
     once: Once,
-    inner: UnsafeCell<Option<Arc<Mutex<T>>>>,
+    inner: UnsafeCell<InnerPointer<T>>,
 }
 
 // The inner value is only used to make an immutable call to `.clone()`. The
@@ -71,7 +102,7 @@ impl<T: Default> Global<T> {
             // is done.
             unsafe {
                 if (*ptr).is_none() {
-                    *ptr = Some(Arc::new(Mutex::new(T::default())));
+                    *ptr = Some(Default::default())
                 }
             }
         });
@@ -85,7 +116,7 @@ impl<T: Default + Send + 'static> Global<T> {
     /// this is not intended to be visible. Once `const fn` is stabilized, this
     /// will become a `const fn`.
     pub const INIT: Global<T> = Global {
-        once: Once::INIT,
+        once: ONCE_INIT,
         inner: UnsafeCell::new(None),
     };
 
@@ -94,44 +125,121 @@ impl<T: Default + Send + 'static> Global<T> {
     /// Prefer the constant value where possible.
     pub fn new() -> Self {
         Self {
-            once: Once::INIT,
+            once: ONCE_INIT,
             inner: UnsafeCell::new(None),
         }
     }
 
-    /// Run a closure on the inner value.
+    /// Run a closure on an immutable reference to the inner value.
     ///
     /// This will return the closure's return type. Internally, [`lock`] is
-    /// called.
+    /// called and unwrapped.
     ///
     /// [`lock`]: #method.lock
+    ///
+    /// # Panics
+    ///
+    /// This will panic if the value is already borrowed mutably in the same
+    /// thread.
     pub fn with<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&T) -> R,
+    {
+        f(&*self.lock().expect("Couldn't immutably access global variable"))
+    }
+
+    /// Run a closure on a mutable reference to the inner value.
+    ///
+    /// This will return the closure's return type. Internally, [`lock_mut`] is
+    /// called and unwrapped.
+    ///
+    /// [`lock_mut`]: #method.lock_mut
+    ///
+    /// # Panics
+    ///
+    /// This will panic if the value is already borrowed either mutably or
+    /// immutably in the same thread.
+    pub fn with_mut<F, R>(&self, f: F) -> R
     where
         F: FnOnce(&mut T) -> R,
     {
-        f(&mut *self.lock())
+        f(&mut *self.lock_mut().expect("Couldn't mutably access global variable"))
     }
 
-    /// Obtain a lock on the inner reference.
+    /// Obtain a lock providing an immutable reference to the inner value0.
     ///
-    /// This method will block the current thread until any other lock held is
-    /// destroyed.
-    pub fn lock(&self) -> GlobalGuard<T> {
+    /// This method will block the current thread until any other threads
+    /// holding a lock are destroyed. If the current thread already has mutable
+    /// access, this method will return an error.
+    pub fn lock(&self) -> Result<GlobalGuard<T>, BorrowFail> {
+        // The borrow checker is not able to prove what we are doing here is
+        // safe. Essentially, the global value lives on the heap and is
+        // guaranteed not to move, so we can treat the inner value as if it is a
+        // 'static reference as long as we make sure to tear down the guard
+        // correctly. For this to work, pointers are used to erase lifetime data
+        // and then a `Drop` implementation manually drops the fields in the
+        // correct order.
+
         // Important: this *must* be called before accessing the inner pointer.
         self.ensure_exists();
 
         // Extra cast to `*const` here is to force us to only use this as a read
         // pointer.
-        let ptr = self.inner.get() as *const Option<_>;
+        let ptr = self.inner.get() as *const InnerPointer<T>;
 
         // This is safe as we already called `ensure_exists`.
-        let opt = unsafe { (*ptr).clone() };
+        let mutex: Arc<_> = unsafe { (*ptr).clone() }.unwrap();
+        let mutex_ptr = &*mutex as *const ReentrantMutex<RefCell<T>>;
 
-        GlobalGuard::new(opt.unwrap())
+        let mutex_guard = unsafe { (*mutex_ptr).lock() };
+        let mutex_guard_ptr = &*mutex_guard as *const RefCell<T>;
+
+        let ref_cell_guard = unsafe {
+            (*mutex_guard_ptr)
+                .try_borrow()
+                .map_err(|_| BorrowFail)?
+        };
+
+        Ok(GlobalGuard {
+            mutex: ManuallyDrop::new(mutex),
+            mutex_guard: ManuallyDrop::new(mutex_guard),
+            ref_cell_guard: ManuallyDrop::new(ref_cell_guard),
+        })
+    }
+
+    /// Obtain a lock providing a mutable reference to the inner value0.
+    ///
+    /// This method will block the current thread until any other threads
+    /// holding a lock are destroyed. If the current thread already has access,
+    /// whether mutable or immutable, this method will return an error.
+    pub fn lock_mut(&self) -> Result<GlobalGuardMut<T>, BorrowFail> {
+        // The body here is largely the same as `lock`. Comments there explain
+        // what is going on.
+        self.ensure_exists();
+
+        let ptr = self.inner.get() as *const InnerPointer<T>;
+
+        let mutex: Arc<_> = unsafe { (*ptr).clone() }.unwrap();
+        let mutex_ptr = &*mutex as *const ReentrantMutex<RefCell<T>>;
+
+        let mutex_guard = unsafe { (*mutex_ptr).lock() };
+        let mutex_guard_ptr = &*mutex_guard as *const RefCell<T>;
+
+        let ref_cell_guard = unsafe {
+            (*mutex_guard_ptr)
+                .try_borrow_mut()
+                .map_err(|_| BorrowFail)?
+        };
+
+        Ok(GlobalGuardMut {
+            mutex: ManuallyDrop::new(mutex),
+            mutex_guard: ManuallyDrop::new(mutex_guard),
+            ref_cell_guard: ManuallyDrop::new(ref_cell_guard),
+        })
     }
 }
 
-/// A handle to some global value.
+/// A mutable handle to some global value.
 ///
 /// This type implements `Deref<Target = T>` and `DerefMut`. Once this guard is
 /// dropped, the global this guard is locking is immediately unlocked.
@@ -140,7 +248,7 @@ impl<T: Default + Send + 'static> Global<T> {
 /// # use global::Global;
 /// static VALUE: Global<i32> = Global::INIT;
 ///
-/// let mut guard = VALUE.lock();
+/// let mut guard = VALUE.lock_mut().unwrap();
 ///
 /// *guard += 1;
 /// *guard += 1;
@@ -148,42 +256,67 @@ impl<T: Default + Send + 'static> Global<T> {
 ///
 /// assert_eq!(*guard, 3);
 /// ```
+pub struct GlobalGuardMut<T: 'static> {
+    // These are marked manually drop to specify drop order. In a perfect world,
+    // the guard would bear the lifetime of the mutex, however that requires
+    // rust to have self-referential structs, which it currently does not have.
+    mutex: ManuallyDrop<Arc<ReentrantMutex<RefCell<T>>>>,
+    mutex_guard: ManuallyDrop<ReentrantMutexGuard<'static, RefCell<T>>>,
+    ref_cell_guard: ManuallyDrop<cell::RefMut<'static, T>>,
+}
+
+impl<T: 'static> Drop for GlobalGuardMut<T> {
+    fn drop(&mut self) {
+        // Drop the guard *before* the mutex.
+        unsafe {
+            ManuallyDrop::drop(&mut self.ref_cell_guard);
+            ManuallyDrop::drop(&mut self.mutex_guard);
+            ManuallyDrop::drop(&mut self.mutex);
+        }
+    }
+}
+
+impl<T: 'static> Deref for GlobalGuardMut<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &*self.ref_cell_guard
+    }
+}
+
+impl<T: 'static> DerefMut for GlobalGuardMut<T> {
+    fn deref_mut(&mut self) -> &mut T {
+        &mut *self.ref_cell_guard
+    }
+}
+
+/// An immutable handle to some global value.
+///
+/// This type implements `Deref<Target = T>`. Once this guard is dropped, the
+/// global this guard is locking is immediately unlocked.
+///
+/// ```
+/// # use global::Global;
+/// static VALUE: Global<i32> = Global::INIT;
+///
+/// assert_eq!(0, *VALUE.lock().unwrap());
+/// ```
 pub struct GlobalGuard<T: 'static> {
     // These are marked manually drop to specify drop order. In a perfect world,
     // the guard would bear the lifetime of the mutex, however that requires
     // rust to have self-referential structs, which it currently does not have.
-    mutex: ManuallyDrop<Arc<Mutex<T>>>,
-    guard: ManuallyDrop<MutexGuard<'static, T>>,
+    mutex: ManuallyDrop<Arc<ReentrantMutex<RefCell<T>>>>,
+    mutex_guard: ManuallyDrop<ReentrantMutexGuard<'static, RefCell<T>>>,
+    ref_cell_guard: ManuallyDrop<cell::Ref<'static, T>>,
 }
 
 impl<T: 'static> Drop for GlobalGuard<T> {
     fn drop(&mut self) {
         // Drop the guard *before* the mutex.
         unsafe {
-            ManuallyDrop::drop(&mut self.guard);
+            ManuallyDrop::drop(&mut self.ref_cell_guard);
+            ManuallyDrop::drop(&mut self.mutex_guard);
             ManuallyDrop::drop(&mut self.mutex);
-        }
-    }
-}
-
-impl<T: 'static> GlobalGuard<T> {
-    /// Construct a new `GlobalGuard` with a reference-counted mutex.
-    fn new(mut mutex: Arc<Mutex<T>>) -> Self {
-        // Both the guard and the mutex are moved into the lock. Rust does not
-        // support self-referential lifetimes so we must use unsafe code here.
-        unsafe {
-            // Remove the lifetime constraints on a borrow.
-            let ptr = &mut mutex as *mut Arc<Mutex<T>>;
-
-            // This should never fail.
-            let guard = (*ptr)
-                .lock()
-                .unwrap();
-
-            GlobalGuard {
-                guard: ManuallyDrop::new(guard),
-                mutex: ManuallyDrop::new(mutex),
-            }
         }
     }
 }
@@ -192,13 +325,7 @@ impl<T: 'static> Deref for GlobalGuard<T> {
     type Target = T;
 
     fn deref(&self) -> &T {
-        &*self.guard
-    }
-}
-
-impl<T: 'static> DerefMut for GlobalGuard<T> {
-    fn deref_mut(&mut self) -> &mut T {
-        &mut *self.guard
+        &*self.ref_cell_guard
     }
 }
 
@@ -221,7 +348,7 @@ mod test {
         for _ in 0..1000 {
             v.push(thread::spawn(|| {
                 for _ in 0..100 {
-                    *NUM.lock() += 1;
+                    *NUM.lock_mut().unwrap() += 1;
                 }
             }));
         }
@@ -230,7 +357,7 @@ mod test {
             thread.join().unwrap();
         }
 
-        assert_eq!(*NUM.lock(), 100_000);
+        assert_eq!(*NUM.lock().unwrap(), 100_000);
     }
 
     // Ensure a lock will block.
@@ -241,7 +368,7 @@ mod test {
         let (tx, rx) = mpsc::channel();
 
         let t1 = thread::spawn(move || {
-            let mut lock = NUM.lock();
+            let mut lock = NUM.lock_mut().unwrap();
 
             // Go.
             tx.send(()).unwrap();
@@ -255,15 +382,77 @@ mod test {
             // Wait for the signal.
             let () = rx.recv().unwrap();
 
-            let mut lock = NUM.lock();
-
-            *lock += 1;
+            *NUM.lock_mut().unwrap() += 1;
         });
-
 
         t1.join().unwrap();
         t2.join().unwrap();
 
-        assert_eq!(*NUM.lock(), 2);
+        assert_eq!(*NUM.lock().unwrap(), 2);
+    }
+
+    #[test]
+    #[should_panic]
+    fn borrow_immutably_while_mutably_borrowed() {
+        static NUM: Global<i32> = Global::INIT;
+
+        let _x = NUM.lock_mut().unwrap();
+        let _y = NUM.lock().unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn borrow_mutably_while_mutably_borrowed() {
+        static NUM: Global<i32> = Global::INIT;
+
+        let _x = NUM.lock_mut().unwrap();
+        let _y = NUM.lock_mut().unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn borrow_mutably_while_immutably_borrowed() {
+        static NUM: Global<i32> = Global::INIT;
+
+        let _x = NUM.lock().unwrap();
+        let _y = NUM.lock_mut().unwrap();
+    }
+
+    #[test]
+    fn borrow_immutably_while_immutably_borrowed() {
+        static NUM: Global<i32> = Global::INIT;
+
+        let _x = NUM.lock().unwrap();
+        let _y = NUM.lock().unwrap();
+    }
+
+    /// Test recursive immutable locking with two interleaved threads.
+    #[test]
+    fn complex_thread_interactions() {
+        static NUM: Global<i32> = Global::INIT;
+
+        let lock1 = NUM.lock().unwrap();
+        let lock2 = NUM.lock().unwrap();
+        let lock3 = NUM.lock().unwrap();
+
+        let t = thread::spawn(|| {
+            *NUM.lock_mut().unwrap() += 1;
+
+            assert!(NUM.lock().is_ok());
+        });
+
+        thread::sleep(Duration::from_millis(100));
+
+        assert!(NUM.lock_mut().is_err());
+
+        drop(lock1);
+        drop(lock2);
+        drop(lock3);
+
+        *NUM.lock_mut().unwrap() += 1;
+
+        t.join().unwrap();
+
+        assert_eq!(2, *NUM.lock().unwrap());
     }
 }
